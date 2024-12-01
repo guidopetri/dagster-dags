4644e2a48d4e22b4db1284c02d7af340a5696d6d
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2024-12-01 12:44:10 -0600

    docker hello world in dagster

---
+++ chess_pipeline/dags.py
@@ -0,0 +1,47 @@
+
+from dagster import (
+    AssetExecutionContext,
+    Backoff,
+    Config,
+    Definitions,
+    Jitter,
+    RetryPolicy,
+    asset,
+    define_asset_job,
+)
+from dagster_docker import execute_docker_container
+
+
+class DagRunConfig(Config):
+    player: str
+    perf_type: str
+    date: str
+    local_stockfish: bool = True
+
+
+all_assets_job = define_asset_job(name='all_assets_job')
+
+
+# TODO: metadata/tags
+@asset(deps=[],
+       code_version='1',
+       retry_policy=RetryPolicy(max_retries=3,
+                                delay=0.2,  # 200ms
+                                backoff=Backoff.EXPONENTIAL,
+                                jitter=Jitter.PLUS_MINUS,
+                                ),
+       )
+def run_docker_hello_world(context: AssetExecutionContext,
+                           config: DagRunConfig,
+                           ) -> None:
+    context.log.info(f'My run ID is {context.run.run_id}')
+    context.log.info(f'{config=}')
+    execute_docker_container(context=context, image='hello-world')
+    return
+
+
+defs = Definitions(
+    assets=[run_docker_hello_world],
+    jobs=[all_assets_job],
+    schedules=[],
+)
